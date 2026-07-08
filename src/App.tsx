643667dc63c6240b99f88459a1dba178aad52c7a
Imraan Unal <imraanunal930@gmail.com>
2026-07-08 20:57:07 +0300

    Daha fazla ornek eklendi

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,9 @@ function App() {
   return(
     <div>
       <Kart basklik = "Kalem" fiyat ={5} aciklama = "Bu bir kalem." />
-      <Kart basklik = "Klavye" fiyat ={500} />
+      <Kart basklik = "Klavye" fiyat ={500} aciklama = "Bu bir kalem."/>
+      <Kart basklik = "Monitor" fiyat ={500} aciklama = "Bu bir kalem."/>
+      <Kart basklik = "Vilgisayar" fiyat ={500} aciklama = "Bu bir kalem."/>
     </div>
   )
 }
